d249c69c25566654c85dcfdd041246f29a00f91f
sandrine <sandrinezocca@gmail.com>
2020-04-06 14:18:12 +0200

    Registering the control in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form-angular';
+  public myForm: FormGroup;
+  
+  ngOnInit() {
+    this.myForm = new FormGroup ({
+      name: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  submit() {
+    console.log(this.myForm);
+  }
 }
